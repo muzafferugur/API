a48593183753aac735bd5f82ffe5dd472a85e597
muzafferugur <av.muzafferugur@gmail.com>
2023-06-05 18:29:26 +0300

    05.06.2023

---
+++ src/test/java/Get06b.java
@@ -0,0 +1,39 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Get06b extends ReqresBaseUrl{
+
+      /*
+       Given
+              https://reqres.in/api/unknown/
+       When
+            I send GET Request to the URL
+       Then
+​
+            1)Status code is 200
+            2)Print all pantone_values
+            3)Print all ids greater than 3 on the console
+              Assert that there are 3 ids greater than 3
+            4)Print all names whose ids are less than 3 on the console
+              Assert that the number of names whose ids are less than 3 is 2
+    */
+    @Test
+    public void get06(){
+        //set the url
+        spec.pathParam("first","unknown");
+        //set the expected data
+
+        //send the request and get the response
+        Response response=given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //do assertion
+        assertEquals(200,response.statusCode());
+        JsonPath jsonPath=response.jsonPath();
+
+    }
+}
