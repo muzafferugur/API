0e4054c5cb4d4a46a4689dd92f00ee08bb90b1d4
muzafferugur <av.muzafferugur@gmail.com>
2023-06-16 22:54:16 +0300

    16.06.2023

--- src/test/java/utilities/Authentication.java
+++ src/test/java/utilities/Authentication.java
@@ -46,5 +46,6 @@ public class Authentication {
         JsonPath token = response.jsonPath();
         return token.getString("id_token");
         // getString("id_token") => Token verir
+
     }
 }
