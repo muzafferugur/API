6b2a0015449c1dd7fc8b037ebb94835789c2ee2d
muzafferugur <av.muzafferugur@gmail.com>
2023-06-22 23:55:30 +0300

    22.06.2023

--- src/test/java/patch_requests/Patch01.java
+++ src/test/java/patch_requests/Patch01.java
@@ -1,10 +1,14 @@
 package patch_requests;
 
 import base_url.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
 
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class Patch01 extends JsonplaceholderBaseUrl {
     /*
@@ -34,6 +38,15 @@ public class Patch01 extends JsonplaceholderBaseUrl {
         //set the expected data
         JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
         Map<String, Object> expectedData = obj.expectedDataMethod(null, "Wash the dishes", null);
+
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the Request and Get the Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().patch("/{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+
 
 
     }
