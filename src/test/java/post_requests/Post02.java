ec3b98b658289c2591083f30fa3cc164e18c6d99
muzafferugur <av.muzafferugur@gmail.com>
2023-06-22 22:48:31 +0300

    22.06.2023

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -1,7 +1,15 @@
 package post_requests;
 
 import base_url.RestfulBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.RestfulTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class Post02 extends RestfulBaseUrl {
 
@@ -22,25 +30,47 @@ public class Post02 extends RestfulBaseUrl {
        I send POST Request to the Url
    Then
        Status code is 200
-       And response body should be like {
-                                           "bookingid": 5315,
-                                           "booking": {
-                                               "firstname": "John",
-                                               "lastname": "Doe",
-                                               "totalprice": 11111,
-                                               "depositpaid": true,
-                                               "bookingdates": {
-                                                   "checkin": "2021-09-09",
-                                                   "checkout": "2021-09-21"
-                                               }
-                                           }
-                                        }
+       And response body should be like
+       {
+    "bookingid": 168,
+    "booking": {
+        "firstname": "John",
+        "lastname": "Doe",
+        "totalprice": 11111,
+        "depositpaid": true,
+        "bookingdates": {
+            "checkin": "2021-09-09",
+            "checkout": "2021-09-21"
+        }
+    }
+}
     */
 
     @Test
     public void post02() {
 
         //set the url
-        spec.pathParam("1","booking");
+        spec.pathParam("first", "booking");
+
+        //set the expected data(payload)
+        RestfulTestData obj = new RestfulTestData();
+        Map<String, String> bookingdatesMap = obj.bookingdatesMethod("2021-09-09", "2021-09-21");
+        Map<String, Object> expectedData = obj.expectedDataMethod("John", "Doe", 11111, true, bookingdatesMap);
+
+        //send the request and get the response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+        response.prettyPrint();
+
+        //do assertion
+        Map<String, Object> actualData=response.as(HashMap.class);//De-Serialization
+        System.out.println(actualData);
+
+
+
+
+
+
+
+
     }
 }
