4a39fb8c383f07fc2b2a60bae9598c210e0509c4
muzafferugur <av.muzafferugur@gmail.com>
2023-06-23 00:18:36 +0300

    23.06.2023

---
+++ src/test/java/get_requests/Get12Pojo.java
@@ -0,0 +1,45 @@
+package get_requests;
+
+import base_url.RestfulBaseUrl;
+import org.junit.Test;
+
+public class Get12Pojo extends RestfulBaseUrl {
+
+    /*
+     Given
+         https://restful-booker.herokuapp.com/booking/18
+     When
+         I send GET Request to the URL
+     Then
+        Status code is 200
+     And
+        Response body is like:
+   {
+    "firstname": "John",
+    "lastname": "Smith",
+    "totalprice": 111,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
+    },
+    "additionalneeds": "Breakfast"
+}
+    */
+
+    @Test
+    public void get12Pojo() {
+        //set the url
+        spec.pathParams("first","booking","second",18);
+
+        //set the expected data
+
+
+
+
+
+
+
+
+    }
+}
