7bc72300273902cfca41a420528949c6944132dc
muzafferugur <av.muzafferugur@gmail.com>
2023-06-23 00:39:43 +0300

    23.06.2023

--- src/test/java/get_requests/Get12Pojo.java
+++ src/test/java/get_requests/Get12Pojo.java
@@ -1,9 +1,12 @@
 package get_requests;
 
 import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
 import pojos.BookingDatesPojo;
 import pojos.BookingPojo;
+
+import static io.restassured.RestAssured.given;
 
 public class Get12Pojo extends RestfulBaseUrl {
 
@@ -32,21 +35,23 @@ public class Get12Pojo extends RestfulBaseUrl {
     @Test
     public void get12Pojo() {
         //set the url
-        spec.pathParams("first","booking","second",18);
+        spec.pathParams("first", "booking", "second", 18);
 
         //set the expected data
         //inner
-        BookingDatesPojo bookingDatesPojo=new BookingDatesPojo("2018-01-01","2019-01-01");
+        BookingDatesPojo bookingDatesPojo = new BookingDatesPojo("2018-01-01", "2019-01-01");
         System.out.println(bookingDatesPojo);
 
         //expected data
-        BookingPojo bookingPojo=new BookingPojo("John","Smith",111,true,bookingDatesPojo,"Breakfast");
-        System.out.println(bookingPojo);
+        BookingPojo expectedData = new BookingPojo("John", "Smith", 111, true, bookingDatesPojo, "Breakfast");
+        System.out.println(expectedData);
 
+        //Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
 
-
-
-
+        //Do Assertion
+        BookingPojo actualData = response.as(BookingPojo.class);
 
 
     }
