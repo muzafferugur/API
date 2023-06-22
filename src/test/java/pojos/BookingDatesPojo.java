7f05d3e79942f75cc78e86050dee0b686fa3c77d
muzafferugur <av.muzafferugur@gmail.com>
2023-06-23 00:23:57 +0300

    23.06.2023

---
+++ src/test/java/pojos/BookingDatesPojo.java
@@ -0,0 +1,34 @@
+package pojos;
+
+public class BookingDatesPojo {
+    //1-)Tüm key'ler için private variable'ler oluşturuyoruz.
+    private String checkin;
+    private String checkout;
+
+    //2-)Tüm parametrelerle parametresiz constructur'lar oluşturuyoruz
+    public BookingDatesPojo(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public BookingDatesPojo() {
+    }
+
+    //3-)Public Getter ve Setter methodlarını oluşturuyoruz.
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+}
