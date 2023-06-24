464982393167d67e2fb4bae42c5679e5896b8b1c
muzafferugur <av.muzafferugur@gmail.com>
2023-06-24 21:28:12 +0300

    24.06.2023

--- src/test/java/post_requests/Post03Pojo.java
+++ src/test/java/post_requests/Post03Pojo.java
@@ -1,4 +1,38 @@
 package post_requests;
 
-public class Post03Pojo {
+import base_url.JsonplaceholderBaseUrl;
+import org.junit.Test;
+
+public class Post03Pojo extends JsonplaceholderBaseUrl {
+
+    /*
+        Given
+           https://jsonplaceholder.typicode.com/todos
+           {
+           "userId": 55,
+           "title": "Tidy your room",
+           "completed": false
+           }
+       When
+           I send POST Request to the Url
+       Then
+           Status code is 201
+       And
+           response body is like {
+                                   "userId": 55,
+                                   "title": "Tidy your room",
+                                   "completed": false,
+                                   "id": 201
+                                   }
+    */
+
+
+    @Test
+    public void post03Pojo() {
+        //set the url
+        spec.pathParam("first", "todos");
+
+        // Set the Expected Data
+
+    }
 }
