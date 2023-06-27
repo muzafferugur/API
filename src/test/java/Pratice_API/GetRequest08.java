abc9d0ca125104ece1e34d76a8f3280c82ed4670
muzafferugur <av.muzafferugur@gmail.com>
2023-06-28 02:15:33 +0300

    28.06.2023

---
+++ src/test/java/Pratice_API/GetRequest08.java
@@ -0,0 +1,21 @@
+package Pratice_API;
+
+import base_url.GMIBankBaseUrl;
+
+public class GetRequest08 extends GMIBankBaseUrl {
+    /*
+          http://www.gmibank.com/api/tp-customers/43703
+
+          "firstName": "Alda",
+          "lastName": "Monahan",
+          "middleInitial": "Nichelle Hermann Kohler",
+          "email": "com.github.javafaker.Name@7c011174@gmail.com",
+          "mobilePhoneNumber": "909-162-8114",
+          "city": "St Louis",
+          "ssn": "108-53-6655"
+
+          1) MATCHERS CLASS
+          2) JSON PATH
+          3) De-Serialization
+    */
+}
