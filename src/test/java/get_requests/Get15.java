ddafc90f17493ff3efdca9c6eb8223e6dc318afa
muzafferugur <av.muzafferugur@gmail.com>
2023-06-27 18:24:45 +0300

    27.06.2023

--- src/test/java/get_requests/Get15.java
+++ src/test/java/get_requests/Get15.java
@@ -1,9 +1,14 @@
 package get_requests;
 
 import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
 import pojos.BookingDatesPojo;
 import pojos.BookingPojo;
+import utils.ObjectMapperUtils;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get15 extends RestfulBaseUrl {
     /*
@@ -34,6 +39,22 @@ public class Get15 extends RestfulBaseUrl {
         //set the expected data
         BookingDatesPojo bookingDatesPojo = new BookingDatesPojo("2018-01-01", "2019-01-01");
         BookingPojo expectedData = new BookingPojo("Josh", "Allen", 111, true, bookingDatesPojo, "midnight snack");
+
+        //send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //do assertion
+        BookingPojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), BookingPojo.class);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getFirstname(), actualData.getFirstname());
+        assertEquals(expectedData.getLastname(), actualData.getLastname());
+        assertEquals(expectedData.getTotalprice(), actualData.getTotalprice());
+        assertEquals(expectedData.getDepositpaid(), actualData.getDepositpaid());
+        assertEquals(expectedData.getBookingdates().getCheckin(), actualData.getBookingdates().getCheckin());
+        assertEquals(expectedData.getBookingdates().getCheckout(), actualData.getBookingdates().getCheckout());
+        assertEquals(expectedData.getAdditionalneeds(), actualData.getAdditionalneeds());
 
 
     }
