67ffb3ef7cbd0651456a22b771b7453d9b59af3b
muzafferugur <av.muzafferugur@gmail.com>
2023-07-07 21:41:26 +0300

    07.07.2023

---
+++ src/test/java/pojos/GMIBankCountryPostPojo.java
@@ -0,0 +1,30 @@
+package pojos;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GMIBankCountryPostPojo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GMIBankCountryPostPojo() {
+    }
+
+    public GMIBankCountryPostPojo(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryPost{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
